be7220596e610494d91fba3f0684b365d672eed8
davay521 <david.vayman@gmail.com>
2015-08-31 18:08:53 -0400

    A

---
+++ Assignment1_Part2.py
@@ -0,0 +1,19 @@
+#assignment1_part2
+class Book(object):
+	author = ""
+	title = ""
+		
+	def __init__(ans,author,title):
+		ans.author = author
+		ans.title = title
+		
+	def display(ans):
+		x = "%s written by %s" % (ans.title,ans.author)
+		print x
+		
+book_1 = Book("Harper Lee", "To Kill a Mockingbird")
+
+book_2 = Book("John Steinbeck", "Of Mice and Men")
+
+book_1.display()
+book_2.display()
